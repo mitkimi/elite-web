db868a4e7ef9718358dcd3578ce030bea297cb33
田昊天 <mitkimi@163.com>
2019-03-12 20:07:45 +0800

    加了高亮插件

--- src/components/nav/component.js
+++ src/components/nav/component.js
@@ -39,7 +39,7 @@ export default {
       } else {
         this.currentPath = `/${mainPath}/*`
       }
-      // console.log(this.currentPath)
+      console.log(this.currentPath)
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -6,13 +6,28 @@ import VueParticles from 'vue-particles'
 import 'element-ui/lib/theme-chalk/index.css'
 import mavonEditor from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
+import VueHighlightJS from 'vue-highlightjs'
 import App from './App'
 import router from './router'
+
+// Highlight.js languages (Only required languages)
+import css from 'highlight.js/lib/languages/css'
+import javascript from 'highlight.js/lib/languages/javascript'
+import vue from 'vue-highlight.js/lib/languages/vue'
+import 'highlight.js/styles/github.css'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.use(VueParticles)
 Vue.use(mavonEditor)
+Vue.use(VueHighlightJS, {
+  // Register only languages that you want
+  languages: {
+    css,
+    javascript,
+    vue
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
